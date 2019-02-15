3f4761baba637b7ad58c9544567671f0e3c6ed1b
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-15 13:50:26 +0100

    +++

--- admin.php
+++ admin.php
@@ -21,10 +21,10 @@ echo "<!DOCTYPE html>
       </script>
 	  </head>";
 
-//$connexion = mysqli_connect("localhost","g1","mdp01")
-//or die ("Tu es nul. Recommence.");
-$connexion = mysqli_connect("localhost","root","")
+$connexion = mysqli_connect("localhost","g1","mdp01")
 or die ("Tu es nul. Recommence.");
+//$connexion = mysqli_connect("localhost","root","")
+//or die ("Tu es nul. Recommence.");
 
 $bd="WebContest";
 
