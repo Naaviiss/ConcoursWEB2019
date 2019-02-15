37410b66f85a581a576f44a2c9035de1b0d8dac5
nanacy <39811762+nanacy@users.noreply.github.com>
2019-02-15 15:35:19 +0100

    ajout deconnexion

---
+++ deconnexion.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    if(isset($_SESSION["id"])){
+        unset($_SESSION["id"]);
+        session_destroy();
+    }
+    if(isset($_SESSION["nom"])){
+        unset($_SESSION["nom"]);
+        session_destroy();
+    }
+    if(isset($_SESSION["status"])){
+        unset($_SESSION["status"]);
+        session_destroy();
+    }
+    header('location: index.html');
+?>
