213ba8df4acd847bcf4f15e3f9f851ed9ec65c18
nanacy <39811762+nanacy@users.noreply.github.com>
2019-02-15 15:55:48 +0100

    css page admin + supp 'nom' de deconnexion

--- admin.php
+++ admin.php
@@ -21,8 +21,8 @@ echo "<!DOCTYPE html>
       </script>
 	  </head>";
 
- $connexion = mysqli_connect("localhost","g1","mdp01")
- or die ("Tu es nul. Recommence.");
+$connexion = mysqli_connect("localhost","g1","mdp01")
+or die ("Tu es nul. Recommence.");
 //$connexion = mysqli_connect("localhost","root","")
 //or die ("Tu es nul. Recommence.");
 
@@ -42,16 +42,16 @@ echo "<tr><td>L'ID de la ressource</td><td>Le nom de la ressource</td><td>La dat
 
 while($ligne=mysqli_fetch_row($resultat)){
 	echo "<tr>";
-	for ($i =1;$i<5;$i++){
+	for ($i =0;$i<4;$i++){
 		//La ligne sur la date
-		if ($i ==4 || $i==3){
+		if ($i ==2 || $i==3){
 			//Est-ce que la date est renseignée?
-			if ($ligne[3] != null){
+			if ($ligne[2] != null){
 				//Si un chercheur a reservé
 				echo "<td>".$ligne[$i]."</td>";
 			}
 			else{
-				echo "<td> Disponible </td>";
+				echo "<td> Vide </td>";
 			}
 
 		}
@@ -60,25 +60,22 @@ while($ligne=mysqli_fetch_row($resultat)){
 		}
 	}
   //Bouton pour supprimer une ressource
-  echo "<td><a href='supprimeressource.php?id=".$i."'>Cliquer ici pour supprimer une ressource</a></td>";
+  echo "<td><a href='supprimeressource.php?id=".$i."'>supprimer</a></td>";
 
 	echo "</tr>";
 
 }
 echo"</table>";
 //Bouton ajouter une ressource
-echo"<button type=\"button\" class=\"btn btn-secondary\" onclick=\"bascule('header1');\">Ajouter une ressource</button>";
+echo "<div id='bouton' onclick=\"bascule('header1');\">Ajouter une ressource</div>";
 echo "<div id='header1' style=\"visibility:hidden;\">
 
 <form method=\"post\" action=\"traitement.php\">
     <p>
         <label for=\"pseudo\">L'id :</label>
-        <select class=\"custom-select\" id=\"inputGroupSelect02\">
-          <option selected>Choisissez une ressource</option>
-          <option value=\"3DPrinter\">3DPrinter</option>
-          <option value=\"DroneXXL360r\">DroneXXL360r</option>
-          <option value=\"Harm256\">Harm256</option>
-        </select></br>
+        <input type=\"text\" name=\"id\" id=\"id\" placeholder=\"Ex : Zozor\" size=\"30\" maxlength=\"10\" /></br>
+        <label for=\"pseudo\">Le nom de la ressource :</label>
+        <input type=\"text\" name=\"nom\" id=\"nom\" placeholder=\"Ex : Zozor\" size=\"30\" maxlength=\"10\" /></br>
     </p>
 </form>
 
@@ -89,7 +86,7 @@ echo "<INPUT TYPE=\"submit\" NAME=\"nom\" VALUE=\" Envoyer \"></div>";
 echo "<h1> Gestion des utilisateurs.</h1>";
 
 
-$requete="select * from Personnel where status ='search'";
+$requete="select * from Personnel where status ='user'";
 $resultat=mysqli_query($connexion,$requete);
 $ligne = 0;
 echo "<table border='1' cellpadding='5' cellpacing='9'>";
@@ -104,6 +101,7 @@ while($ligne=mysqli_fetch_row($resultat)){
 	for ($i = 0;$i<3;$i++){
 			echo "<td>".$ligne[$i]."</td>";
 	}
+  echo $test;
   //Bouton pour supprimer un chercheur
   echo "<td><a href='supprimechercheur.php?id=".$test."'>supprimer</a></td>";
   $compteur=$compteur+1;
@@ -115,7 +113,7 @@ echo"</table>";
 echo "<div id='bouton' onclick=\"bascule('header');\">Ajouter un chercheur</div>";
 echo "<div id='header' style=\"visibility:hidden;\">
 
-<form method=\"post\" action=\"ajouterchercheur.php\">
+<form method=\"post\" action=\"traitement.php\">
     <p>
         <label for=\"pseudo\">L'id :</label>
         <input type=\"text\" name=\"id\" id=\"id\" placeholder=\"Ex : Zozor\" size=\"30\" maxlength=\"10\" /></br>
