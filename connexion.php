c3515146127fa4f0926d6dc1f282da87f22a7820
FloconDavoine <39768042+MarquesFlorian@users.noreply.github.com>
2019-02-15 12:59:49 +0100

    k

--- connexion.php
+++ connexion.php
@@ -1,31 +1,27 @@
 <?php
-session_start();
+// session_start();
 
-//Si deja connecter !
-if (isset($_SESSION['id']) and !empty($_SESSION['id'])){
-	echo "deja connecte";
+// Si deja connecter !
+// if (isset($_SESSION['id']) and !empty($_SESSION['id'])){
+	// echo "deja connecte";
 	// header('location: index.php');
-	exit();
-}
-//Si pas connecter détruit la session et la relance
-else{
-	$_SESSION = Array();
-	session_destroy();
-	session_start();
+	// exit();
+// }
+// Si pas connecter détruit la session et la relance
+// else{
+	// $_SESSION = Array();
+	// session_destroy();
+	// session_start();
 
-}	
+// }
 
-function debug($x){
-	echo "<pre>";
-	print_r($x);
-	echo "</pre>";
-}
+
 ?>
 
 <!DOCTYPE html>
 <head>
 	<meta charset="UTF-8">
-	<title>Suivi des anciens élèves</title>
+	<title>A EXECUTER</title>
 	
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -41,14 +37,13 @@ function debug($x){
 		<div class="row justify-content-center">
 			<div class="col-xs-2 col-sm-9 col-md-7 col-lg-5">
 				<div class="panel panel-default connexion">	
-					<img class='img mb-1'alt='logo UVSQ site Vélizy' src="images/uvsq_iut_velizy_CMJN.jpg" /> 
 					
 					<form action='' method='POST'>
-						<!--
+						
 						<div class="form-group">
-							<h3>Connexion</h3>
+							<h3>Inscription</h3>
 							<h6>Entrez vos identifiants</h6>
-						</div>			-->	
+						</div>			
 
 						<div class="form-group">
 							<input class="form-control" placeholder="Identifiant" name="id" type="text" <?php
@@ -61,10 +56,43 @@ function debug($x){
 							<input class="form-control" placeholder="Mot de passe" name="mdp" type="password" value="" required>
 						</div>
 						
+						<div class="form-group">
+							<input class="form-control" placeholder="Status" name="status" type="text"
+							>
+						</div>
+						
+						<div class='input-group'>
+							<input name='connection' class="btn btn-lg btn-outline-secondary btn-block" type="submit" value="Inscription">
+						</div>
+					</form>
+					<!--
+					<form action='' method='POST'>
+						
+						<div class="form-group">
+							<h3>Connexion</h3>
+							<h6>Entrez vos identifiants</h6>
+						</div>			
+
+						<div class="form-group">
+							<input class="form-control" placeholder="Identifiant" name="id_co" type="text" <?php
+							if (isset($_POST['id'])){
+								echo "value="."'".$_POST['id']."'";
+							}?> required>
+						</div>
+						
+						<div class="form-group">
+							<input class="form-control" placeholder="Mot de passe" name="mdp_co" type="password" value="" required>
+						</div>
+						
 						<div class='input-group'>
 							<input name='connection' class="btn btn-lg btn-outline-secondary btn-block" type="submit" value="Se connecter">
 						</div>
 					</form>
+					-->
+					
+					
+					
+
 				</div>
 			</div>
 		</div>
@@ -72,81 +100,135 @@ function debug($x){
 <body>
 
 <?php
+
+
+
+$serveur = "localhost";
+$login = "root";
+$mdp = "";
+
+//connexion au serveur mysql (ici localhost)
+$connexion=mysqli_connect($serveur,$login,$mdp) 
+or die("Connexion au serveur $serveur impossible pour $login");
+
+
+
+//require("index.php");
+
+
+//nom de la base de donnees
+$bd="webcontest";
+
+//connexion à la base de donnees
+mysqli_select_db($connexion,$bd)
+or die("Impossible d'acceder à la base de données");	
 	
-if (isset($_POST["id"],$_POST["mdp"])){
-	$login = htmlspecialchars($_POST["id"]);
-	// $mdp = hash("sha256",htmlspecialchars($_POST["mdp"]));
-	$mdp = htmlspecialchars($_POST["mdp"]);
+	
+	
+$reqinsert="INSERT into personnel(id,mdp,status)";
+$reqinsert.="VALUES(?,?,?)";
+$reqselect="SELECT mdp from personnel WHERE id=id_co";
+$reqselectid="SELECT id from personnel";
+
+$reqprepare=mysqli_prepare($connexion,$reqinsert);
+
+//liaison des parametres :
+if(isset($_POST['id']) and isset($_POST['mdp']) ){
+	$login=$_POST['id'];
+	$mdp=$_POST['mdp'];
+	$status = 0;
+	// insertion
+	mysqli_stmt_bind_param($reqprepare,'sss',$login,$mdp,$status);
+	mysqli_stmt_execute($reqprepare);
+}
+
+// if(isset($_POST['id_co']) and isset($_POST['mdp'])) {
+	
+//}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+// echo "test";
+	
+// if (isset($_POST["id"],$_POST["mdp"])){
+	// $login = htmlspecialchars($_POST["id"]);
+	// //$mdp = hash("sha256",htmlspecialchars($_POST["mdp"]));
+	// $mdp = htmlspecialchars($_POST["mdp"]);
 		
 	$req = "SELECT * FROM personnel WHERE id like \"$login\" and mdp like \"$mdp\"";
+	// $requeteprep= $connexion->mysqli->prepare("select id,mdp from personnel where id:i and mdp:md");
+	// $requeteprep->bind_param(':i',$id);
+	// $requeteprep->bind_param(':md',$mdp);
 	
-	if ($res = mysqli_query($connexion,$req)){ //test si la commande est bien exec
-		$rowcount=mysqli_num_rows($res);
-		if ($rowcount > 0) { //test si on a des resultats
-			$lig = mysqli_fetch_assoc($res);
-			$_SESSION['id'] = $lig['id'];
-			header('location: index.php');
-			exit();
-		}else{
-			echo '<div class="container">
-					<div class="row justify-content-center">
-					<p>Mot de passe ou identifiant incorrect</p>
-					</div>
-				</div>';
-		}
-	}else{ 
-		echo "Impossible d'acceder a la table personnel !";
-	}
-}
+	// if ($res = mysqli_query($connexion,$req)){ //test si la commande est bien exec
+		// $rowcount=mysqli_num_rows($res);
+		// if ($rowcount > 0) { //test si on a des resultats
+			// $lig = mysqli_fetch_assoc($res);
+			// $_SESSION['id'] = $lig['id'];
+			// header('Location: index.php');
+		// }else{
+			// echo '<div class="container">
+					// <div class="row justify-content-center">
+					// <p>Mot de passe ou identifiant incorrect</p>
+					// </div>
+				// </div>';
+		// }
+	// }else{ 
+		// echo "Impossible d'acceder a la table personnel !";
+	// }
+// }
+
+// else
+	// echo "NON";
 
 
-/**TEST SI IL EXISTE AUCUN COMPTE DANS LA BD**/
+// /**TEST SI IL EXISTE AUCUN COMPTE DANS LA BD**/
 
-$req = "SELECT * FROM personnel";
-if ($res = mysqli_query($connexion,$req)){ //test si la commande est bien exec
-	$rowcount=mysqli_num_rows($res);
-	if ($rowcount <= 0) { //test si on a des resultats
-		echo "Il n'existe pas de compte !";
-		?>
-		<h3>Formulaire de création compte admin</h3>
-		<fieldset>
-		<form action='' method='POST'><table align='center'><tr>
-			<tr><td>LOGIN : </td><td><input type='text' maxsize='10' name='cid' required></td></tr>
-			<tr><td>MDP :</td><td><input type='password' maxsize='10' name='cmdp' required ></td></tr>
-			<tr><td>MDP (confirmation):</td><td><input type='m' maxsize='10' name='cmdpconf' required ></td></tr>
-			
-			<tr><td>type :</td><td> <select name="type">
-									  <option value="user">user</option>
-									  <option value="admin">admin</option>
-									</select></td></tr>
-			
-			<tr><td>&nbsp;</td><td><input type='submit' name='plus' value='crée'></td></tr></table>
-		</form></fieldset></div></table></div></div>	
-		</fieldset>
+// $req = "SELECT * FROM personnel";
+// if ($res = mysqli_query($connexion,$req)){ //test si la commande est bien exec
+	// $rowcount=mysqli_num_rows($res);
+	// if ($rowcount <= 0) { //test si on a des resultats
+		// echo "Il n'existe pas de compte !";
+		// ?>
+		
 		<?php
 		
-		if (isset($_POST["cid"],$_POST["cmdp"],$_POST["cmdpconf"],$_POST["type"]) and $_POST["cmdp"] == $_POST["cmdpconf"]){
-			$reqinsert="INSERT into UTILISATEUR(id,mdp,status)";
-			$reqinsert.="VALUES(?,?,?)";
+		// if (isset($_POST["cid"],$_POST["cmdp"],$_POST["cmdpconf"],$_POST["type"]) and $_POST["cmdp"] == $_POST["cmdpconf"]){
+			// $reqinsert="INSERT into UTILISATEUR(id,mdp,status)";
+			// $reqinsert.="VALUES(?,?,?)";
 
-			$reqprepare=mysqli_prepare($connexion,$reqinsert);
+			// $reqprepare=mysqli_prepare($connexion,$reqinsert);
 
-			//liaison des parametres :
-			if (isset($_POST['cmdp'],$_POST['cmdp'],$_POST['type'])){
-				$login=$_POST['cid'];
-				$mdp= hash("sha256",htmlspecialchars($_POST["cmdp"]));
-				/* $mdp = htmlspecialchars($_POST["cmdp"]); */
-				$droit=0;
+			// //liaison des parametres :
+			// if (isset($_POST['cmdp'],$_POST['cmdp'],$_POST['type'])){
+				// $login=$_POST['cid'];
+				// $mdp= hash("sha256",htmlspecialchars($_POST["cmdp"]));
+				// /* $mdp = htmlspecialchars($_POST["cmdp"]); */
+				// $droit=0;
 
-				// insertion
-				mysqli_stmt_bind_param($reqprepare,'sss',$login,$mdp,$droit);
-				mysqli_stmt_execute($reqprepare);
-			}
-		}
-	}		
-}else{ 
-	echo "Impossible d'acceder a la table UTILISATEUR !";
-}
+// //				insertion
+				// mysqli_stmt_bind_param($reqprepare,'sss',$login,$mdp,$droit);
+				// mysqli_stmt_execute($reqprepare);
+			// }
+		// }
+	// }		
+// }else{ 
+	// echo "Impossible d'acceder a la table UTILISATEUR !";
+// }
 
 
 ?>
