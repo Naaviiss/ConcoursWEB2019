d8f4301d59c98866123f11d26d3fc02eedb286d6
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-15 16:10:08 +0100

    Add files via upload

---
+++ ajoutressource.php
@@ -0,0 +1,30 @@
+<?php
+$serveur = "localhost";
+$login = "g1";
+$mdp = "mdp01";
+
+if(isset($_POST['id']) and isset($_POST['mdp']) )
+{
+	$login=$_POST['id'];
+	$mdp=$_POST['mdp'];
+  $jour = date("y.m.d");
+  $id=$_POST['inputGroupSelect02'];
+
+  //connexion au serveur mysql (ici localhost)
+  $connexion=mysqli_connect($serveur,$login,$mdp)
+  or die("Connexion au serveur $serveur impossible pour $login");
+  //nom de la base de donnees
+  $bd="WebContest";
+  //connexion à la base de donnees
+  mysqli_select_db($connexion,$bd)
+  or die("Impossible d'accéder à la base de données");
+
+  $reqinsert="INSERT into ressource(id,mdp,jour,personne) VALUES(?,?,?,?)";
+  $reqprepare=mysqli_prepare($connexion,$reqinsert);
+  // insertion
+	mysqli_stmt_bind_param($reqprepare,'ssss',$id,$mdp,$jour,$login);
+	mysqli_stmt_execute($reqprepare);
+}
+
+mysqli_close($connexion);
+?>
