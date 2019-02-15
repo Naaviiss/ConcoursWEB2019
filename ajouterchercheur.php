3076e7cfed71d3b8c23987b223c7053dcb375568
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-15 14:36:21 +0100

    sd

--- ajouterchercheur.php
+++ ajouterchercheur.php
@@ -1,7 +1,4 @@
 <?php
-
-
-
 $serveur = "localhost";
 $login = "g1";
 $mdp = "mdp01";
@@ -15,30 +12,26 @@ $bd="WebContest";
 
 //connexion à la base de donnees
 mysqli_select_db($connexion,$bd)
-or die("Impossible d'acceder à la base de données");
+or die("Impossible d'accéder à la base de données");
 
 $reqinsert="INSERT into personnel(id,mdp,status) VALUES(?,?,?)";
 
 $reqprepare=mysqli_prepare($connexion,$reqinsert);
 
-//liaison des parametres :
-if(isset($_POST['connection']))
-{
-	preg_match('/(1)(2)(3)/', $_POST['id'], $matches );
-	var_dump($matches);
-}
-
-
-if( $_POST['status'] > 4)
-	echo "test";
-
-
 if(isset($_POST['id']) and isset($_POST['mdp']) ){
 	$login=$_POST['id'];
 	$mdp=$_POST['mdp'];
-	$status = 0;
-	// insertion
-	mysqli_stmt_bind_param($reqprepare,'sss',$login,$mdp,$status);
-	mysqli_stmt_execute($reqprepare);
+	$reg = '/^search[4-9]|[1-9][0-9])/';
+
+  if(preg_match($reg,$login)){
+    // insertion
+  	mysqli_stmt_bind_param($reqprepare,'sss',$login,$mdp,$status);
+  	mysqli_stmt_execute($reqprepare);
+  }
+	else{
+    echo "Vous ne pouvez pas supprimer ce chercheur";
+  }
 }
+
+mysqli_close($connexion);
 ?>
--- supprimechercheur.php
+++ supprimechercheur.php
@@ -1,20 +1,30 @@
 <?php
-	echo "<h1>Modification de fiche client</h1>";
-  $connexion = mysqli_connect("localhost","g1","mdp01")
-  or die ("Tu es nul. Recommence.");
-  //$connexion = mysqli_connect("localhost","root","")
+  //$connexion = mysqli_connect("localhost","g1","mdp01")
   //or die ("Tu es nul. Recommence.");
+  $connexion = mysqli_connect("localhost","root","")
+  or die ("Tu es nul. Recommence.");
 
   $bd="WebContest";
 
   mysqli_select_db($connexion,$bd)
   or die ("Toujours pas.");
 
-  $chercheur = "search1";
-
   $requete = $connexion -> prepare("DELETE FROM Personnel WHERE id LIKE ?");
-  $requete->bind_param("s",$chercheur);
-  $requete -> execute();
+
+  if(isset($_POST['id']) ){
+  	$login=$_POST['id'];
+  	$reg = '/^search[4-9]|[1-9][0-9])/';
+
+    if(preg_match($reg,$login)){
+      // suppressions
+      $requete->bind_param("s",$id);
+      $requete -> execute();
+    }
+  	else{
+      echo "Vous ne pouvez pas supprimer ce chercheur";
+    }
+  }
+
   $requete->close();
 
 	mysqli_close($connexion);
