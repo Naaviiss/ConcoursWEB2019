5fbb42ca31ff87655466dade3cc928d001f79edc
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-02-15 10:36:10 +0100

    table

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 
 <!DOCTYPE html>
 <?php
-$connexion = mysqli_connect("localhost","g1","mdp01")
+//$connexion = mysqli_connect("localhost","g1","mdp01")
+//or die ("Tu es nul. Recommence.");
+$connexion = mysqli_connect("localhost","root","")
 or die ("Tu es nul. Recommence.");
 
 $bd="WebContest";
@@ -19,28 +21,32 @@ echo "<h1> Voici la table des ressources.</h1>";
 echo "<table border='1' cellpadding='5' cellpacing='9'>";
 while($ligne=mysqli_fetch_row($resultat)){
 	echo "<tr>";
+	for ($i =0;$i<4;$i++){
+			if ($i != 3){
+				echo "<td>".$ligne[$i]."</td>";
+			}
+			else {
+				if ($ligne[2] != null){
+					//Si un chercheur a reservé
+					echo "<td>".$ligne[$i]."</td>";
+				}
+			}
 
-	echo "<td>L'id est : ".$ligne['id']."</td>";
-	echo "<td>Le nom de la ressource est :".$ligne['nom']."</td>";
-	echo "<td>Le jour de réservation est :".$ligne['jour']."</td>";
-  if  ($ligne['jour'] == null){
-		echo "<td>La ressource est réservée par :".$ligne['id_pers']."</td>";
-
+		}
+		echo "</tr>";
 	}
-	echo "</tr>";
-}
 echo"</table>";
 
-
+$ligne = null;
+$requete="select * from Ressource where personne = $id";
+$resultat=mysqli_query($connexion,$requete);
 echo "<h1> Voici la table des ressources réservées par ".$id."</h1>";
 echo "<table border='1' cellpadding='5' cellpacing='9'>";
 while($ligne=mysqli_fetch_row($resultat)){
 	echo "<tr>";
-	if ($id == $ligne['id_pers']){
-		echo "<td>L'id est : ".$ligne['id']."</td>";
-		echo "<td>Le nom de la ressource est :".$ligne['nom']."</td>";
-		echo "<td>Le jour de réservation est :".$ligne['jour']."</td>";
-	}
+	for ($i =0;$i<4;$i++){
+			echo "<td>".$ligne[$i]."</td>";
+		}
 	echo "</tr>";
 }
 echo "</table";
