303c4f5522f28d41fbf59596b55287e9cdb9aef2
Marion <armengaudmarion@gmail.com>
2019-02-15 12:42:37 +0100

    k

--- testrecup.php
+++ testrecup.php
@@ -5,39 +5,22 @@
     $date = date('d-m-Y');
 
     //CONNEXION DB ET RECUPERATION DATA
-    $connexion = mysqli_connect("localhost","g1","mdp01")
+    $connexion = mysqli_connect("localhost","g1","mdp01","WebContest")
         or die ("Erreur lors de la connexion à la base de données");
-    
-    $bd = "WebContest";
 
-    mysqli_select_db($connexion,$bd)
-        or die("Erreur lors de l'accès à la base de données");
 
-    if(!($requeteprep = $connexion->prepare("select id,nom,chercheur from Ressource where date_format(jour,'%d-%m-%Y') in ?"))){
-        echo "la ca marche pa";
-    }
-    
-    if(!$requeteprep->bind_param("s",$date)){
-        echo("la ca marche pas");
-    }
-
-    if(!$requeteprep -> execute()){
-        echo("ca marche pas");
-    }
+    $requete = mysqli_query("select id,nom,chercheur from Ressource where date_format(jour,'%d-%m-%Y') LIKE ".$date);
 
     $data = array();
 
-    $i = 0;
-
-    while($ligne = $requeteprep->get_result()->fetch_row())
+    while($ligne = mysqli_fetch_assoc($requete))
     {
         array_push($data,$ligne);
-        $i++;
     }
 
     var_dump($data);
 
-    $requeteprep -> close();
+    mysqli_free_result($requetep);
 
 
     mysqli_close($connexion);
